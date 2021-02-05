c9aedca6332dd5736dbff6f592bc5b5161d4ea0e
kimho <rlaghekd777@naver.com>
2021-02-06 05:31:48 +0900

    Button 클릭 시 Note 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Note from "./components/Note/index";
+import { useState } from "react";
 
 function App() {
+  const [note, setNote] = useState([]);
+
+  const addNote = () => {
+    setNote(note.concat(<Note />));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={addNote}>note</button>
+      {note}
     </div>
   );
 }
