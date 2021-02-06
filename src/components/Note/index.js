256e55fe65fbdcdf2c1a4ce64989007b2b5aa0cf
witch-factory <soakdma37@gmail.com>
2021-02-06 14:28:44 +0900

    task 1.1

---
+++ src/components/Note/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import App from "../../App";
+
+class Note extends React.Component{
+    render(){
+        return (
+            <div>
+                <textarea>내용을 입력하세요</textarea>
+            </div>
+        )
+
+    }
+}
+
+export default Note;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Note from './components/Note/index.js';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Note />,
   document.getElementById('root')
 );
 
