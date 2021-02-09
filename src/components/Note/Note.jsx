1cace5076569fe479e669216052332fe5a460c38
thgwon-yaho-qcw <gwontaeho96@gmail.com>
2021-02-09 13:02:09 +0900

    Note 삭제 버튼 구현

--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -1,10 +1,17 @@
+import {useState} from 'react'
 import './Note.css'
 
 function Note() {
-    return (
-        <textarea style={{display: "block"}}>
-        아무거나 입력
-        </textarea>
+    const [open, setOpen] = useState(true)
+    return open && (
+        <div>
+            <textarea style={{display: "block"}}>
+            아무거나 입력
+            </textarea>
+            <button onClick={()=>setOpen(false)}>
+                삭제
+            </button>
+        </div>
     )
 }
 
