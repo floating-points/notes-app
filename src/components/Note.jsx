a7a515c0ef22400d72677eeadc5e8be6c7b05ecb
kimho <rlaghekd777@naver.com>
2021-02-13 04:22:50 +0900

    삭제 버튼 추가

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-
-const Note = () => {
-  return <textarea>내용을 입력해주세요</textarea>;
+import "./Note.css";
+const Note = (props) => {
+  //console.log(props.id);
+  return (
+    <div>
+    <textarea>내용을 입력해주세요</textarea>;
+    <button onClick={props.onRemove}>삭제</button>
+    </div>
+  );
 };
 
 export default Note;
