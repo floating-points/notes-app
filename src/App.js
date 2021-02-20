abf43df60ea07d4001b6b4e7337087cb0d605938
S2HA <ohyj607@naver.com>
2021-02-20 23:06:48 +0900

    노트 추가 버튼

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Note from './components/Note';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends Component{
+  state={
+    note : []
+  }
+
+  addNote=()=>{
+    this.setState(
+      ({note})=>({
+      note : [note, {<Note />, id=this.id++}]
+    })
   );
+}
+
+render(){
+  return (
+    <button onClick={this.addNote}>add note</button>
+    {note}
+  );
+}
+
+
 }
 
 export default App;
