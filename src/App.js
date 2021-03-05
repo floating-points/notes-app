68f7eec31583aeb5e5802cb6938741e94dc0b645
hotkimho <rlaghekd777@naver.com>
2021-03-06 03:09:04 +0900

    읽기 수정 및 로컬스토리지 저장

--- src/App.js
+++ src/App.js
@@ -4,19 +4,29 @@ import { useState } from "react";
 function App() {
   const [noteList, setNote] = useState([]);
   const [id, setId] = useState(1);
+  const [noteData, setNoteData] = useState([]);
+
 
   const addNote = () => {
     setNote(() => [...noteList, id]);
     setId(() => id + 1);
   };
-  function removeNote(id){
+
+  const removeNote = (id) =>{
     setNote((noteList) => noteList.filter((note) => note !== id));
   }
+
+
+  const storeNote = (id, text) =>{
+    setNoteData(() => ([...noteData, {id : id, text : text}]));
+    localStorage.setItem("noteData", JSON.stringify(noteData));
+  }
+
   return (
     <div>
       <button onClick={addNote}>Add Note</button>
       {noteList.map((id)  =>(
-      <Note key={id} id={id} removeNote={removeNote} />
+      <Note key={id} id={id} removeNote={removeNote} storeNote={storeNote}/>
       ))}
     </div>
   );
